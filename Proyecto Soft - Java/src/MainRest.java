1d323d3567e0208ef35372f647694930d6f3cac0
JhojanCR <jhojangs27@gmail.com>
2024-05-18 11:28:06 -0500

    Add MainRest y correcciones ortográficas
    Add MainRest y correcciones ortográficas

---
+++ Proyecto Soft - Java/src/MainRest.java
@@ -0,0 +1,14 @@
+
+public class MainRest {
+
+    public static void main(String[] args) {
+
+        Restaurante myLoginRest = new Restaurante(121212, "Frisby",
+                123456789, "Sucursal El Centro, Medellín",
+                "frisby@hotmail.com", "Pollo apanado", "Proceso completado.");
+
+        myLoginRest.mostrarInformacionGeneral();
+
+    }
+
+}
--- Proyecto Soft - Java/src/Restaurante.java
+++ Proyecto Soft - Java/src/Restaurante.java
@@ -4,33 +4,36 @@ public class Restaurante {
 
     int idRestaurante;
     String fullnameRest;
-    double celularRestaurante;
+    int celularRestaurante;
     String direccionRestaurante;
     String correoRestaurante;
     String tipoComida;
+    String infoCompleted;
 
     // Constructor
 
-    public Restaurantes(int idRestaurante, String fullnameRest,int celularRestaurante, String direccionRestaurante, String correoRestaurante, String tipoComida){
+    public Restaurante(int idRestaurante, String fullnameRest,int celularRestaurante,
+                       String direccionRestaurante, String correoRestaurante, String tipoComida,
+                       String infoCompleted){
         this.idRestaurante = idRestaurante;
         this.fullnameRest = fullnameRest;
         this.celularRestaurante = celularRestaurante;
         this.direccionRestaurante = direccionRestaurante;
         this.correoRestaurante = correoRestaurante;
         this.tipoComida = tipoComida;
+        this.infoCompleted = infoCompleted;
     }
 
     // Métodos
 
-    public void mostrarInformacion(){
+    public void mostrarInformacionGeneral(){
         System.out.println("Su código de ingreso es: " + idRestaurante);
-        System.out.println("Bienvenido a su sesión como Restaurante " + fullnameRest);
+        System.out.println("Bienvenido restaurante " + fullnameRest);
         System.out.println("Su especialidad de comida es: " + tipoComida);
         System.out.println("El celular de contacto del restaurante es: " + celularRestaurante);
-        System.out.println("La dirección del restaurante que será visibles para el cliente es: " + direccionRestaurante);
-        System.out.println("El correo eléctrónico que será visible para el cliente es: " + correoRestaurante);
+        System.out.println("La dirección del restaurante que será visible para el cliente es: " + direccionRestaurante);
+        System.out.println("El correo electrónico que será visible para el cliente es: " + correoRestaurante);
+        System.out.println(infoCompleted);
 
     }
-
-
 }
